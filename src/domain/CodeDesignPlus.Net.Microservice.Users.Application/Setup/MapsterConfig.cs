695fc3799ef43638f2d1ddd0f484576f01272765
codedesignplus <codedesignplus@outlook.com>
2025-04-22 22:08:03 -0500

    chroe Update package references to version 0.6.1-beta.1 across multiple projects

--- src/domain/CodeDesignPlus.Net.Microservice.Users.Application/Setup/MapsterConfig.cs
+++ src/domain/CodeDesignPlus.Net.Microservice.Users.Application/Setup/MapsterConfig.cs
@@ -1,7 +1,4 @@
-﻿using CodeDesignPlus.Net.Microservice.Users.Application.Users.Commands.AddProductToUsers;
-using CodeDesignPlus.Net.Microservice.Users.Application.Users.Commands.CancelUsers;
-using CodeDesignPlus.Net.Microservice.Users.Application.Users.Commands.CreateUsers;
-using CodeDesignPlus.Net.Microservice.Users.Application.Users.Commands.UpdateQuantityProduct;
+﻿using CodeDesignPlus.Net.Microservice.Users.Application.Users.Commands.CreateUsers;
 using CodeDesignPlus.Net.Microservice.Users.Domain.ValueObjects;
 
 namespace CodeDesignPlus.Net.Microservice.Users.Application.Setup;
