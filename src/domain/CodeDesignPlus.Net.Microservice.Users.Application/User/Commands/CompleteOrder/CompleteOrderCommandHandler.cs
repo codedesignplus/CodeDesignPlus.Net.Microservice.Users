53bbcf67240aad2dd03b1d53be7c2a2fb720b318
Wilzon Camilo Liscano Galindo <wliscano@codedesignplus.com>
2026-02-13 14:52:29 -0500

    fix: update package versions to 0.13.0-beta.12718 across multiple project files and remove unused CompleteOrder command and handler

--- src/domain/CodeDesignPlus.Net.Microservice.Users.Application/User/Commands/CompleteOrder/CompleteOrderCommandHandler.cs
+++
@@ -1,9 +0,0 @@
-namespace CodeDesignPlus.Net.Microservice.Users.Application.User.Commands.CompleteOrder;
-
-public class CompleteOrderCommandHandler(IUserRepository repository, IUserContext user, IPubSub pubsub) : IRequestHandler<CompleteOrderCommand>
-{
-    public Task Handle(CompleteOrderCommand request, CancellationToken cancellationToken)
-    {
-        return Task.CompletedTask;
-    }
-}
